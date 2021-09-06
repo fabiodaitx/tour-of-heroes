82b75baa4e6f1fd6fb91dd512499d8fea727882e
Fábio Fabian Daitx <fabio.daitx@petrobras.com.br>
2021-09-06 10:58:50 -0300

    App Tour of Heroes

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero.model';
+
+export const environment = {
+  production: true,
+  baseUrl: '/api',
+};
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes: Hero[] = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'Windstorm' },
+    ];
+
+    return { heroes };
+  }
+
+  genId(heroes: Hero[]): number {
+    // retorna apenas o id dos heroes: [11, 12, 13, 14, 15, 16]
+    const heroIds = heroes.map((hero) => hero.id);
+
+    // retorna o maior valor dos heroIds: Math.max(11, 12, 13, 14, 15, 16)
+    const maxId = Math.max(...heroIds);
+
+    // incrementa o maxId ou retorna 1 caso nao tenha heroes no array
+    const nextId = heroes && heroes.length > 0 ? maxId + 1 : 1;
+
+    return nextId;
+  }
+}
